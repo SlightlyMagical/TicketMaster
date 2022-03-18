404965a18e45094bea32cc6fd1203a2a1e0b35af
m-eunbi <mariahansen.91@hotmail.com>
2022-03-18 14:51:35 +0100

    Prepared createEvent view for code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,25 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("gui/CreateEventView.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+
+        primaryStage.setTitle("Event Master");
+
+        primaryStage.setResizable(false);
+
+        primaryStage.show();
+    }
 }
--- src/gui/CreateEventController.java
+++ src/gui/CreateEventController.java
@@ -1,4 +1,35 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class CreateEventController {
+
+    @FXML
+    private Label lblTitle;
+    @FXML
+    private TextField txtEventName;
+    @FXML
+    private TextField txtLocation;
+    @FXML
+    private DatePicker dpStartDate;
+    @FXML
+    private TextField txtStartTime;
+    @FXML
+    private DatePicker dpEndDate;
+    @FXML
+    private TextField txtEndTime;
+    @FXML
+    private TextArea txtDescription;
+    @FXML
+    private TextArea txtLocalGuidance;
+    @FXML
+    private Button btnConfirm;
+
+    public void cancelAction(ActionEvent actionEvent) {
+    }
+
+    public void confirmAction(ActionEvent actionEvent) {
+    }
 }
